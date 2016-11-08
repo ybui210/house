074e69d2138343848b39d2288261c1b354b328ff
Yen Bui <ybui0210@gmail.com>
2016-11-07 16:16:37 -0800

    moved the house to the right

--- js/house.js
+++ js/house.js
@@ -7,54 +7,42 @@ var x = drawHouse();
 function drawHouse() {
 
 	drawBackground();
-	drawShadow();
+	
 	drawBase();
-	drawRoof();
 	drawChimney();
+	drawRoof();
+	
 	drawDoor();
+
 
 }
 //GRASS
 function drawBackground () {
-
-ctx.fillStyle="#76FF03";
-ctx.fillRect(0,250,500,250);
 	ctx.fillStyle="#40C4FF";
 	ctx.fillRect(0,0,500,500);
 	ctx.fillStyle="#76FF03";
 	ctx.fillRect(0,250,500,250);
 }
-function drawShadow() {
-	ctx.beginPath();
-	ctx.moveTo(210,440);
-	ctx.lineTo(400,440);
-	ctx.lineTo(540,340);
-	ctx.lineTo(300,340);
-	ctx.closePath();
-	ctx.fillStyle = "#64DD17";
-	ctx.fill();
-}
 
 //DRAW BASE
 function drawBase() {
-
 	ctx.beginPath();
-	ctx.moveTo(110,320);
-	ctx.lineTo(155,260);
-	ctx.lineTo(210,340);
-	ctx.lineTo(210,440);
-	ctx.lineTo(110,420);
-	ctx.lineTo(110,320);
+	ctx.moveTo(60,320);
+	ctx.lineTo(105,260);
+	ctx.lineTo(160,340);
+	ctx.lineTo(160,440);
+	ctx.lineTo(60,420);
+	ctx.lineTo(60,320);
 	ctx.closePath();
 	ctx.fillStyle="#C62828";
 	ctx.fill();
 
 	ctx.beginPath();
-	ctx.moveTo(210,340);
-	ctx.lineTo(330,300);
-	ctx.lineTo(330,400);
-	ctx.lineTo(210,440);
-	ctx.lineTo(210,340);
+	ctx.moveTo(160,340);
+	ctx.lineTo(280,300);
+	ctx.lineTo(280,400);
+	ctx.lineTo(160,440);
+	ctx.lineTo(160,340);
 	ctx.closePath();
 	ctx.fillStyle="#B71C1C";
 	ctx.fill();
@@ -63,55 +51,56 @@ function drawBase() {
 //DRAW ROOF
 function drawRoof () {
 	ctx.beginPath();
-	ctx.moveTo(150,250);
-	ctx.lineTo(90,325);
-	ctx.lineTo(100,335);
-	ctx.lineTo(155,260);
-	ctx.lineTo(150,250);
+	ctx.moveTo(100,250);
+	ctx.lineTo(40,325);
+	ctx.lineTo(50,335);
+	ctx.lineTo(105,260);
+	ctx.lineTo(100,250);
 	ctx.closePath();
 	ctx.fillStyle = "#3E2723";
 	ctx.fill();
 
 	ctx.beginPath();
-	ctx.moveTo(150,250);
-	ctx.lineTo(300,200);
-	ctx.lineTo(350,300);
-	ctx.lineTo(200,350);
-	ctx.lineTo(150,250);
+	ctx.moveTo(100,250);
+	ctx.lineTo(250,200);
+	ctx.lineTo(300,300);
+	ctx.lineTo(150,350);
+	ctx.lineTo(100,250);
 	ctx.closePath();
 	ctx.fillStyle = "#212121"
 	ctx.fill();
+
+	
 }
 
 //DRAW CHIMNEY
 function drawChimney() {
-
 	ctx.beginPath();
-	ctx.moveTo(270,260);
-	ctx.lineTo(300,250);
-	ctx.lineTo(300,190);
-	ctx.lineTo(270,200);
-	ctx.lineTo(270,260);
+	ctx.moveTo(170,260);
+	ctx.lineTo(200,250);
+	ctx.lineTo(200,190);
+	ctx.lineTo(170,200);
+	ctx.lineTo(170,260);
 	ctx.closePath();
 	ctx.fillStyle = "#BF360C";
 	ctx.fill();
 
 	ctx.beginPath();
-	ctx.moveTo(270,200);
-	ctx.lineTo(270,260);
-	ctx.lineTo(250,240);
-	ctx.lineTo(250,185);
-	ctx.lineTo(270,200);
+	ctx.moveTo(170,200);
+	ctx.lineTo(170,260);
+	ctx.lineTo(150,240);
+	ctx.lineTo(150,185);
+	ctx.lineTo(170,200);
 	ctx.closePath();
 	ctx.fillStyle = "#F4511E";
 	ctx.fill();
 
 	ctx.beginPath();
-	ctx.moveTo(250,185);
-	ctx.lineTo(270,200);
-	ctx.lineTo(300,190);
-	ctx.lineTo(280,175);
-	ctx.lineTo(250,185);
+	ctx.moveTo(150,185);
+	ctx.lineTo(170,200);
+	ctx.lineTo(200,190);
+	ctx.lineTo(180,175);
+	ctx.lineTo(150,185);
 	ctx.closePath();
 	ctx.fillStyle = "#990000";
 	ctx.fill();
@@ -121,11 +110,11 @@ function drawChimney() {
 
 function drawDoor() {
 	ctx.beginPath();
-	ctx.moveTo(180,435);
-	ctx.lineTo(130,425);
-	ctx.lineTo(130,360);
-	ctx.lineTo(180,370);
-	ctx.lineTo(180,435);
+	ctx.moveTo(130,435);
+	ctx.lineTo(80,425);
+	ctx.lineTo(80,360);
+	ctx.lineTo(130,370);
+	ctx.lineTo(130,435);
 	ctx.closePath();
 	ctx.fillStyle = "#ffcc00";
 	ctx.fill();
